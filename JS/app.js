9a92da141b207616508677341a9cc5bb180832fb
AliShiyyab <alishiyyab1998@gmail.com>
2021-04-14 15:26:25 -0700

    Create function will uses in button, Create a new game

--- JS/app.js
+++ JS/app.js
@@ -3,7 +3,6 @@ let Person = prompt("Hello there! Who am I talking to?");
 alert("Greetings dear " + Person + "\n" + "I hope you're enjoying your time" + "\n" + "My family name is Al-Kiswani")
 
 let city = prompt("Based on my family name, Can you guess where am I from? yes or no").toLowerCase()
-
 switch (city)
 {
     case "yes": 
@@ -12,23 +11,22 @@ switch (city)
     case "no":
         alert("No worries. We are from Jerusalem");
         break;
-        default:
-            alert("Really! it was a yes or no question 😏 ")
+    default:
+        alert("Really! it was a yes or no question 😏 ")
 
 }
 
 let webPage = prompt("Do you think that I built this web page on my own? yes or no").toUpperCase()
-switch (webPage)
-{
-    case "YES": 
-        alert("Ooh! then you know I am a developer! Amazing 😉");
-        break;
-    case "NO":
-        alert("Ooch! that hurts! don't you think I can do it!!? 😔");
-        break;
-        default:
-            alert("Agaain! it was a YES or NO question 😔😏 ")
-}            
+
+if(webPage.toUpperCase() == 'YES'){ 
+    alert("Ooh! then you know I am a developer! Amazing 😉");
+}
+else if (webPage.toUpperCase() == 'NO'){
+    alert("Ooch! that hurts! don't you think I can do it!!? 😔");
+}
+else{
+    alert("Agaain! it was a YES or NO question 😔😏 ")
+}  
 
 let myBackGround = prompt("So, do you think that I studied coding at the university? yes or no").toUpperCase()
 switch (myBackGround)
@@ -71,43 +69,51 @@ switch (final)
 
 
 let tries;
-
 for (tries = 3; tries >= 0; tries--) {
     let born = prompt("Guess which year I was born in?");
-    if (born < 1986){
-        alert ("That's too old for my age. " + "you have " + tries + "attempt(s) left")
+    if (born < 1986 && tries != 0){
+        alert ("That's too old for my age. " + "you have " + tries + "attempt(s) left");
+        continue;
     }
-    else if (born > 1986){
-        alert("That's too young for my age. " + "you have " + tries + "attempt(s) left")
+    else if (born > 1986 && tries != 0){
+        alert("That's too young for my age. " + "you have " + tries + "attempt(s) left");
+        continue;
     }
-    else if(tries == 0){
-        alert("Okay that's it. I was born in 1986")
-    }
-    else{
-        alert("Spectacular! how did you know that!")
-    }
-
-}    
-
-let attempts;
-
-for (attempts = 5; attempts >= 0; attempts--) {
-    let idea = prompt("Hey TA! This is question number 7, not finished yet!" + "\n" + "still working on it");
-    if (idea < 1986){
-        alert ("That's too old for my age. " + "you have " + attempts + "attempt(s) left")
-    }
-    else if (idea > 1986){
-        alert("That's too young for my age. " + "you have " + attempts + "attempt(s) left")
-    }
-    else if(attempts == 0){
-        alert("Okay that's it. I was born in 1986")
+    else if (tries == 0){
+        alert ("You have lost all attempts\nthat's it, I was born in 1986 \nGood luck in other time.");
+        break;
     }
     else{
         alert("Spectacular! how did you know that!")
+        break;
+    }    
+
+}
+
+let arr = ['A' , 'E' , 'M' , 'O' , 'T' , 'K'];
+alert("yOu are plAying with me ! \n" +
+"I'll AsK you about my favourit English leTter \n"+
+"PleasE Enter a captial letter and you have 8 atteMpts");
+let counter = 0;
+for (var i = 0 ; i < 8 ; i++){
+    let test = prompt("Enter a Capital letter : ");
+    for (var j = 0 ; j < 6 ; j++){
+        if (test == arr[i]){
+            counter += 1;
+            break;
+        }
+        else{
+            continue;
+        }
     }
+}
+alert("Before seeing your marks, I'll talk about my favourit letter.\n"+
+"you ask himself about where ? i using my favourit letter in my first alert when i ask you about play or not."+
+"\nyour marks is " + counter + " of 6");
 
-}    
-
+function Talk_to_me(){
+    location.href = "https://www.instagram.com/alkiswani_moe/" ;
+}
 
 console.log ("who this is " + Person)
 console.log ("Did you guess my hometown " + city)
@@ -115,3 +121,4 @@ console.log ("Did I build this web " + webPage)
 console.log ("Guessed my education " + myBackGround)
 console.log ("believe I started at 34 " + age)
 console.log ("is this my friend "  + final);
+
